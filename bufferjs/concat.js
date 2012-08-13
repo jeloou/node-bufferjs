48a79c38ba57119254aca76917a1f27bcbdfd24b
Joseph Núñez <toctochello@gmail.com>
2012-08-13 13:29:59 -0430

    Fixing indentation. Optional callback and "limit" method to set "totalLength"

--- bufferjs/concat.js
+++ bufferjs/concat.js
@@ -1,31 +1,49 @@
 (function () {
-  "use strict";
+    "use strict";
 
-  function concat(bufs) {
-    if (!Array.isArray(bufs)) {
-      bufs = Array.prototype.slice.call(arguments);
-    }
+    var limit = function(n) {
+	if ( typeof n !== 'number' || n > this.length ) {
+	    return this
+	}
 
-    var bufsToConcat = [], length = 0;
-    bufs.forEach(function (buf) {
-      if (buf) {
-        if (!Buffer.isBuffer(buf)) {
-          buf = new Buffer(buf);
-        }
-        length += buf.length;
-        bufsToConcat.push(buf);
-      }
-    });
+	return this.slice(0, n);
 
-    var concatBuf = new Buffer(length), index = 0;
-    bufsToConcat.forEach(function (buf) {
-      buf.copy(concatBuf, index);
-      index += buf.length;
-    });
+    };
 
-    return concatBuf;
-  }
+    var concat = function(buffers) {
+
+	var args = Array.prototype.slice.call(arguments);
+	var callback = (typeof args[args.length-1] === 'function')? args.pop() : function() {};
+	var buffers = ( args.length > 0 && Array.isArray(args[0]) )? args.shift() : args;
+
+	var _buffers = [], length = 0;
+	buffers.forEach(function (buffer) {
+	    if ( !buffer ) {
+		return; 
+	    }
+	    
+	    if ( !Buffer.isBuffer(buffer) ) {
+		buffer = new(Buffer)(buffer);
+	    }
+	    length += buffer.length;
+	    _buffers.push(buffer);
+	    
+	});
+	buffers = _buffers;
+	
+	var concat_buffer = new(Buffer)(length), index = 0;
+	buffers.forEach(function(buffer) {
+	    buffer.copy(concat_buffer, index);
+	    index += buffer.length;
+
+	});
+	concat_buffer.limit = limit;
+	callback.call(concat_buffer, concat_buffer);
+
+	return concat_buffer;
+    };
   
-  Buffer.concat = concat;
+
+    Buffer.concat = concat;
 
 }());
